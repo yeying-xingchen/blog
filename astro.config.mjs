41764113cbedee319ebb489fe99191cdb83163da
Yeying-Xingchen <yeyingxingchen@yeah.net>
2026-01-29 14:53:16 +0800

    ci: :construction_worker: 自动部署到github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: siteConfig.site,
+  base: '/blog',
   integrations: [tailwind(), sitemap()],
   markdown: {
     rehypePlugins: [
